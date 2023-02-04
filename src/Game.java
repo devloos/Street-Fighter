e9e5d46389628e1b85ca81de284164fb81546fc1
Carlos Aguilera <18aguilerac@gmail.com>
2023-02-04 12:08:01 -0800

    main update

--- src/Game.java
+++ src/Game.java
@@ -1,18 +1,32 @@
 package src;
 
+import java.io.IOException;
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Game extends Application {
+  // DOING THIS FOR NOW WE WILL CHANGE THIS
+  public static final String BASE_FILE_URI = "<ABSOLUTE FOLDER PATH>";
 
   @Override
   public void start(Stage window) throws Exception {
     window.setTitle("Tic Tac Toe");
-    Scene scene = new Scene(new Label(), 640, 480);
-    window.setScene(scene);
-    window.show();
+    window.setResizable(false);
+
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(new URL(BASE_FILE_URI + "/src/views/<FXML FILE>"));
+
+    try {
+      Scene scene = loader.<Scene>load();
+      window.setScene(scene);
+      window.show();
+    } catch (IOException e) {
+      System.out.println(e);
+    }
   }
 
   public static void main(String[] args) {
