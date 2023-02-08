4c629798aa33353f6a8017d333b92f4142cf1169
Connor Darling <connordarling@me.com>
2023-02-07 20:01:01 -0800

    connected multiplayer to game board

--- src/main/java/edu/saddleback/tictactoe/controllers/LoginController.java
+++ src/main/java/edu/saddleback/tictactoe/controllers/LoginController.java
@@ -11,12 +11,14 @@ import edu.saddleback.tictactoe.Game;
 
 import java.util.Random;
 
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
 
 public class LoginController {
   // all fxml properties
@@ -25,6 +27,7 @@ public class LoginController {
   public Button multiplayer_btn = null;
   public Button return_btn = null;
   public AnchorPane overlay = null;
+  public AnchorPane root = null;
   public ImageView background_image = null;
 
   public LoginController() {
@@ -42,6 +45,18 @@ public class LoginController {
 
     single_player_btn.setOnAction(event -> {
       overlay.setVisible(true);
+    });
+
+    multiplayer_btn.setOnAction(event -> {
+      try {
+        FXMLLoader loader = new FXMLLoader(Game.class.getResource("views/Game.fxml"));
+        Scene scene = loader.<Scene>load();
+        Stage window = (Stage)root.getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+      } catch (Exception e) {
+        System.out.println(e);
+      }
     });
 
     online_btn.setOnAction(event -> {
