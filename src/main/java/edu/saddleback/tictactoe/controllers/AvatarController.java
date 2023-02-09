3fa2be9e50bb2a59a5614856f430ba3752cbe0db
Carlos Aguilera <18aguilerac@gmail.com>
2023-02-08 18:31:24 -0800

    lint

--- src/main/java/edu/saddleback/tictactoe/controllers/AvatarController.java
+++ src/main/java/edu/saddleback/tictactoe/controllers/AvatarController.java
@@ -5,26 +5,25 @@ import java.net.URL;
 
 import edu.saddleback.tictactoe.Game;
 import edu.saddleback.tictactoe.models.Player;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.BooleanBinding;
-import javafx.beans.value.ObservableBooleanValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class AvatarController {
     // fxml properties
     public HBox p1 = null;
     public HBox p2 = null;
+    public AnchorPane error = null;
+    public Button error_btn = null;
+    public Text error_message = null;
 
     private HBox previousP1 = null;
     private HBox previousP2 = null;
@@ -32,6 +31,13 @@ public class AvatarController {
     public AvatarController() {
         player1 = new Player("Carlos");
         player2 = new Player("Professor");
+    }
+
+    @FXML
+    public void initialize() {
+        error_btn.setOnAction(event -> {
+            error.setVisible(false);
+        });
     }
 
     // will use setAvatarPath for player1
@@ -96,58 +102,17 @@ public class AvatarController {
 
     @FXML
     public void switchToGameScreen(ActionEvent event) throws IOException {
-        AnchorPane sameAvatar = (AnchorPane) (((Node) event.getSource()).getScene().lookup("#sameAvatar"));
-        AnchorPane noAvatar = (AnchorPane) (((Node) event.getSource()).getScene().lookup("#noAvatar"));
-        Button return_no = (Button) (((Node) event.getSource()).getScene().lookup("#return_no"));
-        Button return_same = (Button) (((Node) event.getSource()).getScene().lookup("#return_same"));
-        GridPane grid1 = (GridPane) (((Node) event.getSource()).getScene().lookup("#grid1"));
-        GridPane grid2 = (GridPane) (((Node) event.getSource()).getScene().lookup("#grid2"));
-        HBox p1 = (HBox) (((Node) event.getSource()).getScene().lookup("#p1"));
-        HBox p2 = (HBox) (((Node) event.getSource()).getScene().lookup("#p2"));
-        
-        sameAvatar.setVisible(false);
-        noAvatar.setVisible(false);
-        return_no.setVisible(false);
-        return_same.setVisible(false);
-
-        return_no.setOnAction(eventReturn -> {
-            noAvatar.setVisible(false);
-            return_no.setVisible(false);
-            grid1.setVisible(true);
-            grid2.setVisible(true);
-            p1.setVisible(true);
-            p2.setVisible(true);
-        });
-
-        return_same.setOnAction(eventReturn -> {
-            sameAvatar.setVisible(false);
-            return_same.setVisible(false);
-            grid1.setVisible(true);
-            grid2.setVisible(true);
-            p1.setVisible(true);
-            p2.setVisible(true);
-        });
-
         if (player1.getAvatarPath() == null || player2.getAvatarPath() == null) {
-            noAvatar.setVisible(true);
-            return_no.setVisible(true);
-            grid1.setVisible(false);
-            grid2.setVisible(false);
-            p1.setVisible(false);
-            p2.setVisible(false);
+            error_message.setText("Please choose an avatar");
+            error.setVisible(true);
             return;
         }
 
-        if(player1.getAvatarPath().equals(player2.getAvatarPath())) {
-            sameAvatar.setVisible(true);
-            return_same.setVisible(true);
-            grid1.setVisible(false);
-            grid2.setVisible(false);
-            p1.setVisible(false);
-            p2.setVisible(false);
+        if (player1.getAvatarPath().equals(player2.getAvatarPath())) {
+            error_message.setText("Please choose different avatars");
+            error.setVisible(true);
             return;
         }
-
 
         FXMLLoader loader = new FXMLLoader(Game.class.getResource("views/Game.fxml"));
         loader.setController(new GameController(player1, player2));
