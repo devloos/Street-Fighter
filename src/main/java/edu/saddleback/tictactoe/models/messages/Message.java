bbd1b08c5dde5e478831a708f97d67fdbd555c75
Carlos Aguilera <18aguilerac@gmail.com>
2023-04-02 17:34:54 -0700

    jesus

--- src/main/java/edu/saddleback/tictactoe/models/messages/Message.java
+++ src/main/java/edu/saddleback/tictactoe/models/messages/Message.java
@@ -3,6 +3,31 @@ package edu.saddleback.tictactoe.models.messages;
 import java.io.Serializable;
 
 public class Message implements Serializable {
-  public MessageType type = null;
-  public String message = null;
+  public Message(MessageType type) {
+    this.type = type;
+  }
+
+  public Message(MessageType type, String message) {
+    this.type = type;
+    this.message = message;
+  }
+
+  public MessageType getType() {
+    return type;
+  }
+
+  public void setType(MessageType type) {
+    this.type = type;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  private MessageType type = null;
+  private String message = null;
 }
