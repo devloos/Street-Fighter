2f488e2c46cc01a598ad1e1616e9825e4a9f3a5a
Carlos Aguilera <18aguilerac@gmail.com>
2023-04-02 17:38:32 -0700

    jesus

--- src/main/java/edu/saddleback/tictactoe/models/messages/Received.java
+++ src/main/java/edu/saddleback/tictactoe/models/messages/Received.java
@@ -1,5 +1,23 @@
 package edu.saddleback.tictactoe.models.messages;
 
 public class Received extends Message {
-  public boolean success;
+  public Received(boolean success) {
+    super(MessageType.Received);
+    this.success = success;
+  }
+
+  public Received(boolean success, String message) {
+    super(MessageType.Received, message);
+    this.success = success;
+  }
+
+  public boolean isSuccess() {
+    return success;
+  }
+
+  public void setSuccess(boolean success) {
+    this.success = success;
+  }
+
+  private boolean success = false;
 }
